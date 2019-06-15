a318c928888e5c1d17feacfc0c87523c8c1c9541
Josh Crane <josh.crane512@gmail.com>
2019-06-15 11:28:00 +0800

    Go tree building and python matrix

---
+++ python/matrix/matrix.py
@@ -0,0 +1,16 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+
+        # Convert a string into a list of lists
+        matrix_rows = matrix_string.split("\n")
+        matrix_rows = [row.split(" ") for row in matrix_rows]
+        matrix_rows = [[int(i) for i in row] for row in matrix_rows]
+
+        assert len(set([len(row) for row in matrix_rows])) == 1, "Unbalanced matrix, rows not uniform length"
+        self.matrix = matrix_rows
+
+    def row(self, index):
+        return self.matrix[index - 1]
+
+    def column(self, index):
+        return [row[index - 1] for row in self.matrix]
